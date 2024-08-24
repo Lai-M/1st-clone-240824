43dded2d53b3dec1af845a2229445bd66d19c574
Lai-M <lai376199@gmail.com>
2024-08-24 06:14:59 +0000

    1

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("hello")
+print("to replace http-server")
+
+from flask import request, render_template, Flask
+
+app=Flask(__name__)
+@app.route("/")
+def index():
+    return(render_template("index.html"))
+
+
+if __name__ == "__main__":
+    app.run()
